3b4b487bad9c3b00bb16f86efb288d7c148957d3
David King <David.King@open.ac.uk>
2013-08-08 18:56:16 +0100

    sort_brat supersedes brat_sorter supports new fragmented entity feature of brat
    Signed-off-by: David King <David.King@open.ac.uk>

--- sort_brat.py
+++ sort_brat.py
@@ -1,31 +1,36 @@
 '''Sort entities in a brat stand-off file.
 
 Editing annotations in brat simply amends the annotation file. New and changed
-entities can be added at the end fo teh existing entities.
+entities can be added at the end of the existing entities.
 For some of our tasks, it is easier if the entities are listed in offset order.
 This script has two functions: one to read and sort on start offset the
 entities in a brat file returning a list of tuples, and one to write the list
 of tuples to a file inserting new IDs in acending order.
 
+The script can be run on an individual file, or on all .ann files in a folder.
+
 David King <David.King@open.ac.uk>
 for ViBRANT <http://vbrant.eu//>
-March 2013
+August 2013
+
+This version tweaked to support new fragmented entity feature of brat.
 
 License: GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
 '''
 
 from collections import namedtuple
+import os
 import re
 
 Entity = namedtuple(
     'Entity',
-    'brat_ID, brat_type, start_offset, end_offset, brat_text')
+    'brat_ID, brat_type, start_offset, other_offsets, brat_text')
 
 extract_stand_off = re.compile(
     '(?P<brat_ID>\S+)\t'
     '(?P<brat_type>\S+)\W'
     '(?P<start_offset>\d+)\W'
-    '(?P<end_offset>\d+)\t'
+    '(?P<other_offsets>[\d; ]+)\t'
     '(?P<brat_text>.*$)')
 
 def sort_entities(filename):
@@ -37,7 +42,7 @@ def sort_entities(filename):
                 brat_ID=stand_off.group('brat_ID'),
                 brat_type=stand_off.group('brat_type'),
                 start_offset=int(stand_off.group('start_offset')),
-                end_offset=int(stand_off.group('end_offset')),
+                other_offsets=stand_off.group('other_offsets'),
                 brat_text=stand_off.group('brat_text')))
     entities.sort(key=lambda t: t.start_offset)
     return entities
@@ -50,22 +55,23 @@ def write_entities(entities, filename):
                 'T' + str(idx) + '\t' +
                 entity.brat_type + ' ' +
                 str(entity.start_offset) + ' ' +
-                str(entity.end_offset) + '\t' +
+                entity.other_offsets + '\t' +
                 entity.brat_text + '\n')
+
+def sort_file(file_name):
+    sorted_entities = sort_entities(file_name)
+    write_entities(sorted_entities, file_name)
+
+def sort_folder(folder_name):
+    ann_files = (entry for entry in os.listdir(folder_name) if entry.endswith('ann'))
+    for ann_file in ann_files:
+        file_name = folder_name + '/' + ann_file
+        sorted_entities = sort_entities(file_name)
+        write_entities(sorted_entities, file_name)
+
 
 if __name__ == '__main__':
 
-    # sample uses
+    sort_file('aves_v1/d149.ann')
 
-    # for a one off file sort
-    # entity_list = sort_entities('bca_03_aves_v1/bca_03_aves_v1_p053.ann')
-    # print(repr(entity_list)) # see what is returned
-    # write_entities(entity_list, 'bca_03_aves_v1/bca_03_aves_v1_p053_sorted.ann')
-
-    # for a complete directory
-    import os
-    directory = 'bca_03_aves_v1'
-    ann_files = [entry for entry in os.listdir(directory) if entry.endswith('ann')]
-    for ann_file in ann_files:
-        sorted_entities = sort_entities(directory + '/' + ann_file)
-        write_entities(sorted_entities, directory + '/' + ann_file)
+    # sort_folder('aves_v1')
